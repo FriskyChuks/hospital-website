199b21c17f93e24d9ad78464857cb0f2a4fcb71e
MedTech TCRC <chuksedwin.ce@gmail.com>
2024-01-21 15:14:28 +0100

    nuild file and requirements file updates

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,7 @@ pip install -r requirements.txt
 
 # make migrations
 echo "migrating databases..."
+python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
 echo "collecting static files..."
