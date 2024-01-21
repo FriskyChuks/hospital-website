7157ab233b13a6b7617a035a9b18450bb40ccbb3
MedTech TCRC <chuksedwin.ce@gmail.com>
2024-01-21 15:26:54 +0100

    build_files.sh updated

--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,9 @@ echo "building project packages..."
 pip install -r requirements.txt
 
 # make migrations
-echo "migrating databases..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+# echo "migrating databases..."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
 
-echo "collecting static files..."
-python3.9 manage.py collectstatic --noinput
+# echo "collecting static files..."
+# python3.9 manage.py collectstatic --noinput
