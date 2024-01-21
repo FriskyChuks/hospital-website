c10c46265fb525a73e456ada37283a5a21a98fc5
MedTech TCRC <chuksedwin.ce@gmail.com>
2024-01-21 15:30:21 +0100

    build_files.sh modified for static files

--- build_files.sh
+++ build_files.sh
@@ -7,5 +7,5 @@ pip install -r requirements.txt
 # python3.9 manage.py makemigrations --noinput
 # python3.9 manage.py migrate --noinput
 
-# echo "collecting static files..."
-# python3.9 manage.py collectstatic --noinput
+echo "collecting static files..."
+python3.9 manage.py collectstatic --noinput
